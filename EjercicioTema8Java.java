9f7ced36552434ffdf617bc03db6a5ea7bdadcdd
Sandro Sousa <105113293+sandrosousarivera@users.noreply.github.com>
2023-02-21 13:55:24 +0100

    Create EjercicioTema8Java.java

---
+++ EjercicioTema8Java.java
@@ -0,0 +1,13 @@
+public static void main (String[] args){
+
+    Main myObject = new Main ();
+
+    public class Persona {
+
+        private String edad;
+        private String nombre;
+        private int telefono;
+
+    }
+
+}
