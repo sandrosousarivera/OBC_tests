2835a4abcfe51cc5bc0b2c0e41b5fc56e9547476
Sandro Sousa <105113293+sandrosousarivera@users.noreply.github.com>
2023-02-27 15:06:27 +0100

    Create javaBasicoEjercicio1.java
    ejercicio 1 del curso de java basico de open bootcamp

---
+++ javaBasicoEjercicio1.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      
+        String hola= "Estos son los tipos de variable que voy a imprimir; Integer, Long, Double, Boleano:";
+        int numero = 1;
+        long numeroLargo = -263;
+        double numeroDecimal = 2.4;
+        boolean verdad = true;
+
+        System.out.println(hola+ "," +numero+ "," +numeroLargo+ "," +numeroDecimal+ "," + verdad);
+    }
+
+
+}
